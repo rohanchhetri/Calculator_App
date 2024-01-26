e755a6be1f7c7614fc4c3274ae840235d6b0d558
rohanchhetri <102142929+rohanchhetri@users.noreply.github.com>
2024-01-26 13:56:51 -0500

    Update script.js

--- script.js
+++ script.js
@@ -110,20 +110,3 @@ equalBtn.addEventListener("click", () => {
   calculateResult();
   updateDisplay();
 });
-// additionBtn.addEventListener("click", () => selectOperator("+"));
-// additionBtn.addEventListener("click", () => selectOperator("+"));
-
-// if (e.target.innertHTML == "=") {
-//   string = eval(string);
-//   input.value = string;
-// } else if (e.target.innerHTML == "AC") {
-//   string = "";
-//   input.value = string;
-// } else if (e.target.innerHTML == "DEL") {
-//   string = string.substring(0, string.length - 1);
-//   input.value = string;
-// } else {
-//   string += e.target.innerHTML;
-//   input.value = string;
-// }
-// return string;
